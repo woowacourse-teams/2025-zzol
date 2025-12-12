04f321776f40a12265ebbd4013ea03d0311dd48a
leehyeonsu <leehyeonsu4888@gmail.com>
2025-12-12 18:38:42 +0900

    refactor: RedisPubSubListener네이밍 변경

--- backend/src/main/java/coffeeshout/global/redis/pubsub/RedisPubSubListener.java
+++ backend/src/main/java/coffeeshout/global/redis/pubsub/RedisPubSubListener.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class AbstractRedisPubSubListener implements MessageListener {
+public class RedisPubSubListener implements MessageListener {
 
     private final ObjectMapper objectMapper;
     private final RedisMessageListenerContainer redisMessageListenerContainer;

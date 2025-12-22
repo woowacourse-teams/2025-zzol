188f5ed46d90855887d45959006ee731dec9f805
leehyeonsu <leehyeonsu4888@gmail.com>
2025-12-23 00:54:26 +0900

    refactor: key이름 통일

--- backend/src/main/java/coffeeshout/global/redis/stream/StreamKey.java
+++ backend/src/main/java/coffeeshout/global/redis/stream/StreamKey.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum StreamKey {
-    ROOM_BROADCAST("room:broadcast"),
+    ROOM_BROADCAST("room"),
     ROOM_JOIN("room:join"),
-    CARD_GAME_SELECT_BROADCAST("cardgame:select:broadcast"),
-    MINIGAME_EVENTS("minigame:events"),
-    RACING_GAME_EVENTS("racinggame:events");
+    CARD_GAME_SELECT_BROADCAST("cardgame:select"),
+    MINIGAME_EVENTS("minigame"),
+    RACING_GAME_EVENTS("racinggame");
 
     private final String redisKey;
 }

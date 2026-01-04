251bfd39757c0156ff75705adbac8f900e242e14
MinJun Choi <mj043000@naver.com>
2026-01-04 19:54:25 +0900

    refactor: RedisStreamEventRegistrar 제거 및 BaseEvent 서브타입 등록 방식 변경 (#1049)
    - RedisStreamEventRegistrar 클래스 제거 및 관련 로직 ObjectMapperConfig로 이전
    - Reflections 라이브러리를 활용한 BaseEvent 서브타입 등록 방식 추가
    - build.gradle.kts에 Reflections 라이브러리 의존성 추가

--- backend/src/main/java/coffeeshout/global/redis/ObjectMapperConfig.java
+++ backend/src/main/java/coffeeshout/global/redis/ObjectMapperConfig.java
@@ -2,18 +2,37 @@ package coffeeshout.global.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class ObjectMapperConfig {
 
-    @Bean(name = "redisObjectMapper")
-    public ObjectMapper objectMapper() {
-        final ObjectMapper mapper = new ObjectMapper();
+    private static final String BASE_PACKAGE = "coffeeshout";
 
+    @Bean(name = "redisObjectMapper")
+    public ObjectMapper redisObjectMapper() {
+        final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
 
+        registerBaseEventSubtypes(mapper);
+
         return mapper;
+    }
+
+    private void registerBaseEventSubtypes(ObjectMapper mapper) {
+        final Reflections reflections = new Reflections(BASE_PACKAGE);
+        final Set<Class<? extends BaseEvent>> eventTypes = reflections.getSubTypesOf(BaseEvent.class);
+
+        for (Class<? extends BaseEvent> eventType : eventTypes) {
+            mapper.registerSubtypes(eventType);
+            log.debug("Registered event subtype: {}", eventType.getSimpleName());
+        }
+
+        log.info("Registered {} BaseEvent subtypes for Redis serialization", eventTypes.size());
     }
 }

0024040e73ffb451f200f72a88f4b8177600aa54
MinJun Choi <mj043000@naver.com>
2026-03-14 22:36:03 +0900

    style: LobbyPage 스타일 구조 재설계
    - SectionContent 컴포넌트 분리 및 Wrapper 스타일 수정
    - LobbyPage 레이아웃 유연성 및 유지보수성 향상

--- frontend/src/features/room/lobby/pages/LobbyPage.styled.ts
+++ frontend/src/features/room/lobby/pages/LobbyPage.styled.ts
@@ -1,17 +1,17 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   height: 100%;
+`;
+
+export const SectionContent = styled.div`
+  flex: 1;
   overflow-y: auto;
-  padding-bottom: 4rem;
 `;
 
 export const Wrapper = styled.div`
-  position: sticky;
-  bottom: 1rem;
-  margin: 0 2rem;
-  z-index: 1;
+  flex-shrink: 0;
+  padding: 0.75rem 2rem;
 `;

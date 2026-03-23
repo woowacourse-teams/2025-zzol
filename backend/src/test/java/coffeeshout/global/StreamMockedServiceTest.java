7b3966da751dc7101f3c52aff47c423f246ab57d
7i.w__k <tizmfns3@gmail.com>
2026-03-24 00:33:55 +0900

    test: add MockitoSpyBean for DelayedRoomRemovalService in StreamMockedServiceTest

--- backend/src/test/java/coffeeshout/global/StreamMockedServiceTest.java
+++ backend/src/test/java/coffeeshout/global/StreamMockedServiceTest.java
@@ -1,11 +1,15 @@
 package coffeeshout.global;
 
 import coffeeshout.global.redis.stream.StreamPublisher;
+import coffeeshout.room.application.service.DelayedRoomRemovalService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 public abstract class StreamMockedServiceTest extends ServiceTest {
 
     @MockitoBean
     protected StreamPublisher streamPublisher;
 
+    @MockitoSpyBean
+    protected DelayedRoomRemovalService delayedRoomRemovalService;
 }

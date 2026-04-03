56ba360f0fcb9ff805b2dee006809e77bf4cb058
7i.w__k <tizmfns3@gmail.com>
2026-04-04 00:35:19 +0900

    fix: 불필요한 import 제거

--- frontend/src/features/miniGame/blockStackingGame/components/EliminatedOverlay/EliminatedOverlay.tsx
+++ frontend/src/features/miniGame/blockStackingGame/components/EliminatedOverlay/EliminatedOverlay.tsx
@@ -1,4 +1,3 @@
-import Headline1 from '@/components/@common/Headline1/Headline1';
 import * as S from './EliminatedOverlay.styled';
 import chatBubble from '@/assets/chat_bubble.svg';
 import coffee from '@/assets/logo/coffee-white.png';
